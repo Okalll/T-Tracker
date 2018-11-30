9696cb1c88568277ee313c93e45801bd1b379f23
Okall Vivian <vivieokall@gmail.com>
2018-11-30 16:19:02 +0300

    Modify html modify js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function(T-Tracker){
-  var first=[parseInt(documentGetByClass("first")).value]
-  var second=[parseInt(documentGetByClass("second")).value]
-  var third=[parseInt(documentGetByClass("third")).value]
-}
+var triangleTracker = function(){
+  var first = document.getElementById("one").value;
+  var second = document.getElementById("two").value;
+  var third = document.getElementById("three").value;
+
+  if (first === second && second === third && first === third){
+    alert("Equilateral Triangle");
+  } else if(first === second || second === third || first === third){
+    alert("Isosceles Triangle");
+  } else if((first + second) <= (third) || (first +third) <= (second) (second + third) <= (first)){
+    alert("Scalene Triangle");
+  } else {
+    alert("NOT a triangle");
+  }
+};
