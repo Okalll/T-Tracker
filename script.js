019a95d2e473d92ad6bcafac05381dc95ae40bbf
Okall Vivian <vivieokall@gmail.com>
2018-11-30 15:02:01 +0300

    Add js

---
+++ script.js
@@ -0,0 +1,5 @@
+function(T-Tracker){
+  var first=[parseInt(documentGetByClass("first")).value]
+  var second=[parseInt(documentGetByClass("second")).value]
+  var third=[parseInt(documentGetByClass("third")).value]
+}
