7771e6777201be7db2b4fada6eff82939565170f
Okall Vivian <vivieokall@gmail.com>
2018-12-05 11:39:20 +0300

    Update script.js Update README.md Add BDD

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 ## Built by OKALL VIVIAN.
 ## Description
 A Triangle Tracker is an application used for inputting figures to come up with three different types of triangles.The three types to be tested here are:Equilateral Triangle, Isosceles Triangle and Scalene Triangle.
+## BDD
+| Behaviour | input | Output |
+| :-------- | :---- | :----- |
+| If three sides are equal | 5,5,5 | Equilateral Triangle |
+| If all sides are not equal | 2,3,4  | Scalene triangle |
+
 ## Set-up/Installation
 Since it cannot be installed all you can do is to fork it and clone it through my Github.
 ## Site on Github
--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 var triangleTracker = function(){
-  var first = document.getElementById("one").value;
-  var second = document.getElementById("two").value;
-  var third = document.getElementById("three").value;
-
+  var first = parseInt(document.getElementById("one").value);
+  var second = parseInt(document.getElementById("two").value);
+  var third = parseInt(document.getElementById("three").value);
+if (first+second>third && second+third>first && first+third>second){
   if (first === second && second === third && first === third){
     alert("Equilateral Triangle");
-  } else if(first === second || second === third || first === third){
+  }else if(first == second || second == third || first == third){
     alert("Isosceles Triangle");
-  } else if((first + second) <= (third) || (first +third) <= (second) (second + third) <= (first)){
+  }else{
     alert("Scalene Triangle");
-  } else {
-    alert("NOT a triangle");
   }
+}else if((first + second) <= (third) || (first +third) <= (second) || (second + third) <= (first)){
+  alert("Not a Triangle");
+}else if(isNaN(first) || isNaN(second) || isNaN(third)){
+  alert("Not a Number");
+}
 };
